16fd9401c5c69ab5a7ad0842ecf00c8947ba5807
Mathias Lafeldt <misfire@debugon.org>
2009-08-06 08:26:43 +0000

    debugger: refactor code a little bit; remove libkernel dir.

--- ee/debugger/debugger.c
+++ ee/debugger/debugger.c
@@ -46,43 +46,128 @@ char *erl_dependancies[] = {
 
 #define GS_BGCOLOUR *((volatile unsigned long int*)0x120000e0)
 
-int (*OldSifSetReg)(u32 register_num, int register_value) = NULL;
 extern void OrigSifSetReg(u32 register_num, int register_value);
 extern void HookSifSetReg(u32 register_num, int register_value);
-void NewSifSetReg(u32 register_num, int register_value);
-int PostReset_Hook(void);
-
 extern int __NR_OrigSifSetReg;
-static int set_reg_hook = 0;
 
-/* debugger ready state, to know if we can check remote cmd from IOP */
+static int (*OldSifSetReg)(u32 register_num, int register_value) = NULL;
+static int set_reg_hook = 0;
 static int debugger_ready = 0;
+static u8 g_buf[80*1024] __attribute__((aligned(64)));
+
+
+typedef struct {
+	u32	hash;
+	u8	*addr;
+	u32	size;
+} irxent_t;
+
+/* TODO: make this configurable */
+#define IRX_ADDR	0x80030000
+
+#define HASH_PS2DEV9	0x0768ace9
+#define HASH_PS2IP	0x00776900
+#define HASH_PS2SMAP	0x0769a3f0
 
 /*
- * replacement function for SifSetReg().
+ * load_module_from_kernel - Load IOP module from kernel RAM.
+ */
+static int load_module_from_kernel(u32 hash, u32 arg_len, const char *args)
+{
+	const irxent_t *irx_ptr = (irxent_t*)IRX_ADDR;
+	int irxsize = 0, ret;
+
+	DI();
+	ee_kmode_enter();
+
+	/*
+	 * find module by hash and copy it to user memory
+	 */
+	while (irx_ptr->hash) {
+		if (irx_ptr->hash == hash) {
+			irxsize = irx_ptr->size;
+			memcpy(g_buf, irx_ptr->addr, irxsize);
+			break;
+		}
+		irx_ptr++;
+	}
+
+	ee_kmode_exit();
+	EI();
+
+	/* not found */
+	if (!irxsize)
+		return -1;
+
+	/* load module from buffer */
+	SifExecModuleBuffer(g_buf, irxsize, arg_len, args, &ret);
+	return ret;
+}
+
+/*
+ * post_reboot_hook - Will be executed after IOP reboot to load our modules.
+ */
+static int post_reboot_hook(void)
+{
+	int ret;
+
+	/* init services */
+	GS_BGCOLOUR = 0xff0000;
+	SifInitRpc(0);
+	SifInitIopHeap();
+	SifLoadFileInit();
+
+	GS_BGCOLOUR = 0xffff00;
+
+	/* load our modules from kernel */
+	ret = load_module_from_kernel(HASH_PS2DEV9, 0, NULL);
+	if (ret < 0)
+		while (1) ;
+	ret = load_module_from_kernel(HASH_PS2IP, 0, NULL);
+	if (ret < 0)
+		while (1) ;
+	ret = load_module_from_kernel(HASH_PS2SMAP, 0, NULL);
+	if (ret < 0)
+		while (1) ;
+
+	GS_BGCOLOUR = 0x0000ff;
+
+	/* deinit services */
+	SifExitRpc();
+	SifExitIopHeap();
+	SifLoadFileExit();
+
+	GS_BGCOLOUR = 0x000000;
+
+#ifdef DISABLE_AFTER_IOPRESET
+	_fini();
+#endif
+	return 1;
+}
+
+/*
+ * NewSifSetReg - Replacement function for SifSetReg().
  */
 void NewSifSetReg(u32 regnum, int regval)
 {
-	/* perform original SifSetReg */
+	/* call original SifSetReg() */
 	OrigSifSetReg(regnum, regval);
 
-	/* catch new IOP reboot */
-	if ((regnum == SIF_REG_SMFLAG) && (regval == 0x10000)) {
+	/* catch IOP reboot */
+	if (regnum == SIF_REG_SMFLAG && regval == 0x10000) {
 		debugger_ready = 0;
 		set_reg_hook = 4;
 	}
 
 	if (set_reg_hook) {
 		set_reg_hook--;
-		if (set_reg_hook == 0) {
-			/* IOP reboot done */
-			if ((regnum == 0x80000000) && (regval == 0)) {
-				/* IOP sync */
-				while (!(SifGetReg(SIF_REG_SMFLAG) & 0x40000))
-					;
-				/* reload extra modules */
-				//PostReset_Hook();
-			}
+		/* check if reboot is done */
+		if (!set_reg_hook && regnum == 0x80000000 && !regval) {
+			/* IOP sync */
+			while (!(SifGetReg(SIF_REG_SMFLAG) & 0x40000))
+				;
+			/* load our modules */
+			post_reboot_hook();
 		}
 	}
 }
@@ -113,99 +198,10 @@ int _fini(void)
 }
 
 /*
- * This function is constantly called by the cheat engine.
+ * debugger_loop - Constantly called by the cheat engine.
  */
 int debugger_loop(void)
 {
+	/* TODO: handle remote debugging here */
 	return 0;
-}
-
-
-typedef struct {
-	u32	hash;
-	u8	*addr;
-	u32	size;
-} irxent_t;
-
-static u8 g_buf[80*1024] __attribute__((aligned(64)));
-static u8 *g_irx_buf = g_buf;
-
-/* TODO: make this configurable */
-#define IRX_ADDR 0x80030000
-
-#define HASH_PS2DEV9	0x0768ace9
-#define HASH_PS2IP	0x00776900
-#define HASH_PS2SMAP	0x0769a3f0
-
-static int load_module_from_kernel(u32 hash, u32 arg_len, const char *args)
-{
-	const irxent_t *irx_ptr = (irxent_t*)IRX_ADDR;
-	int irxsize = 0, ret;
-
-	DI();
-	ee_kmode_enter();
-
-	/*
-	 * find module by hash and copy it to user memory
-	 */
-	while (irx_ptr->hash) {
-		if (irx_ptr->hash == hash) {
-			irxsize = irx_ptr->size;
-			memcpy(g_irx_buf, irx_ptr->addr, irxsize);
-			break;
-		}
-		irx_ptr++;
-	}
-
-	ee_kmode_exit();
-	EI();
-
-	/* not found */
-	if (!irxsize)
-		return -1;
-
-	/* load module */
-	SifExecModuleBuffer(g_irx_buf, irxsize, arg_len, args, &ret);
-	return ret;
-}
-
-/*
- * PostReset_Hook: function to be executed after IOP reset to reload needed modules
- */
-int PostReset_Hook(void)
-{
-	int ret;
-
-	/* Init RPC/IopHeap/loadfile services */
-	GS_BGCOLOUR = 0xff0000;
-	SifInitRpc(0);
-	SifInitIopHeap();
-	SifLoadFileInit();
-
-	GS_BGCOLOUR = 0xffff00;
-
-	/* load additional modules */
-	ret = load_module_from_kernel(HASH_PS2DEV9, 0, NULL);
-	if (ret < 0)
-		while (1) ;
-	ret = load_module_from_kernel(HASH_PS2IP, 0, NULL);
-	if (ret < 0)
-		while (1) ;
-	ret = load_module_from_kernel(HASH_PS2SMAP, 0, NULL);
-	if (ret < 0)
-		while (1) ;
-
-	GS_BGCOLOUR = 0x0000ff;
-
-	/* de-Init RPC/IopHeap/loadfile services */
-	SifExitRpc();
-	SifExitIopHeap();
-	SifLoadFileExit();
-
-	GS_BGCOLOUR = 0x000000;
-
-#ifdef DISABLE_AFTER_IOPRESET
-	_fini();
-#endif
-	return 1;
 }
