76fd3fd39aa3fa69847d75231ebf215180314a52
Mathias Lafeldt <misfire@debugon.org>
2009-10-21 20:33:57 +0200

    Rename Artemis to ps2rd.

--- ee/loader/mystring.h
+++ ee/loader/mystring.h
@@ -3,20 +3,20 @@
  *
  * Copyright (C) 2009 misfire <misfire@xploderfreax.de>
  *
- * This file is part of Artemis, the PS2 game debugger.
+ * This file is part of ps2rd, the PS2 remote debugger.
  *
- * Artemis is free software: you can redistribute it and/or modify
+ * ps2rd is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Artemis is distributed in the hope that it will be useful,
+ * ps2rd is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Artemis.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ps2rd.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _MYSTRING_H_
