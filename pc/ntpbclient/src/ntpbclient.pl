4f5c98258305b32447a27da5ef6f1af94da67552
Mathias Lafeldt <misfire@debugon.org>
2010-05-11 23:18:23 +0200

    More work on ntpbclient.pl

--- pc/ntpbclient/src/ntpbclient.pl
+++ pc/ntpbclient/src/ntpbclient.pl
@@ -9,7 +9,7 @@ use warnings;
 use strict;
 use IO::Socket;
 
-my $remote_host = "192.168.0.1";
+my $remote_host = "192.168.0.10";
 my $remote_port = 4234;
 
 my $REMOTE_CMD_HALT = 0x201;
@@ -20,20 +20,21 @@ print "target: $remote_host:$remote_port\n";
 my $sock = IO::Socket::INET->new(
 	PeerAddr => $remote_host,
 	PeerPort => $remote_port,
-	Proto => "tcp"
+	Proto => "tcp",
+	Timeout => 3
 ) or die "Could not create socket: $!\n";
 
 my @magic = (0xff, 0x00, "NTPB");
 my $size = 0;
+my $cmd = $REMOTE_CMD_RESUME;
 
-my $cmd;
-if ($ARGV[0] eq "halt") {
-	$cmd = $REMOTE_CMD_HALT;
-} else {
-	$cmd = $REMOTE_CMD_RESUME;
-}
+my $buf = pack("C2 a4 S S", @magic, $size, $cmd);
 
-my $buf = pack("C2 a4 S2", @magic, $size, $cmd);
+$sock->send($buf) or die "Send error: $!\n";
+
+#$sock->recv($buf, 65536) or die "Recv error: $!\n";
+
+$buf = pack("C2 a4 S S S", @magic, $size, $cmd, 1);
 
 $sock->send($buf) or die "Send error: $!\n";
 
