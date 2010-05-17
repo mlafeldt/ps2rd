9ab3694f8fda8ac63dad14489795aff5de7f98f2
Mathias Lafeldt <misfire@debugon.org>
2010-05-17 15:23:25 +0200

    Add copyright to ntpbclient.pl.

--- pc/ntpbclient/src/ntpbclient.pl
+++ pc/ntpbclient/src/ntpbclient.pl
@@ -1,8 +1,23 @@
 #!/usr/bin/perl
 #
-# Perl rewrite of ntpbclient
+# $Id$
 #
 # Copyright (C) 2010 Mathias Lafeldt <misfire@debugon.org>
+#
+# This file is part of PS2rd, the PS2 remote debugger.
+#
+# PS2rd is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PS2rd is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PS2rd.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 use warnings;
