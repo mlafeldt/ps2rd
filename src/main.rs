94998242e71a83cf3e6fde78ed16bfda1df5a243
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2017-08-23 15:49:31 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    let mut stream = TcpStream::connect("192.168.0.10:4234").unwrap();
+
+    stream.write_all(b"\xff\x00NTPB\x00\x00\x01\x02").unwrap();
+
+    let mut buf = [0; 65536];
+    stream.read(&mut buf).unwrap();
+    println!("{:?}", buf.len());
+
+    stream.write_all(b"\xff\x00NTPB\x00\x00\xff\xff").unwrap();
 }
